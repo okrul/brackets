4cdbdc400fa1d91f806c5e919375b7da5ad0f2bd
Aliaksandr Okrul <okrul@list.ru>
2021-03-15 19:58:51 +0300

    feat: implement JS: Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 
+  let map = new Map();
+
+  bracketsConfig.forEach(element => {
+    map.set(element[0], element[1]);  
+  });
+
+  let arr = [];
+  arr.push(str[0]);
+
+  for (let i = 1; i < str.length; i ++)
+  {
+    if (str[i] === map.get(arr[arr.length - 1]))
+      {arr.pop();}
+    else
+      {arr.push(str[i]);}
+  }  
+
+  return arr.length == 0;
+
 }
+
